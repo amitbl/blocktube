e01bb923a41b5f59d183817aee8432807518fee2
Amit <4527660+amitbl@users.noreply.github.com>
2018-05-17 23:46:11 +0300

    Inject seed script

--- src/scripts/content_script.js
+++ src/scripts/content_script.js
@@ -1,90 +1,111 @@
-let globalStorage;
-let compiledStorage;
-let ready = false;
-const port = chrome.runtime.connect();
+(function () {
+  'use strict';
 
-const utils = {
-  sendStorage() {
-    window.postMessage({ from: 'BLOCKTUBE_CONTENT', type: 'storageData', data: compiledStorage || globalStorage }, `https://${document.domain}`);
-  },
-  inject() {
-    const s = document.createElement('script');
-    s.src = chrome.extension.getURL('src/scripts/inject.js');
-    s.async = false;
-    (document.head || document.documentElement).prepend(s);
-  },
-}
-
-const storage = {
-  set(data) {
-    chrome.storage.local.set({ storageData: data });
-  },
-  get(cb) {
-    chrome.storage.local.get('storageData', (storageRes) => {
-      cb(storageRes.storageData);
-    });
+  // Do not run on already opened YouTube tabs
+  if (document.body) {
+    console.info('Please refresh this tab to activate BlockTube');
+    return;
   }
-}
 
-const blockHandlers = {
-  channelId(data) {
-    return [
-      `// Blocked by context menu (${data[0].text})`,
-      data[0].navigationEndpoint.browseEndpoint.browseId,
-      '',
-    ];
-  },
-  videoId(data) {
-    return [
-      `// Blocked by context menu (${data.title.simpleText})`,
-      data.videoId,
-      '',
-    ];
-  }
-};
+  // Inject seed
+  const seed = document.createElement('script');
+  seed.textContent = '{SEED_CONTENTS}';
+  seed.async = false;
+  (document.head || document.documentElement).prepend(seed);
 
-const events = {
-  contextBlock(data) {
-    const entries = blockHandlers[data.type](data.info);
-    globalStorage.filterData[data.type].push(...entries);
-    storage.set(globalStorage);
-  },
-  ready() {
-    if (compiledStorage) utils.sendStorage();
-    ready = true;
-  }
-};
+  let globalStorage;
+  let compiledStorage;
+  let ready = false;
+  const port = chrome.runtime.connect();
 
-// Listen for messages from background page
-port.onMessage.addListener((msg) => {
-  switch (msg.type) {
-    case 'filtersData': {
-      if (msg.data) {
-        globalStorage = msg.data.storage;
-        compiledStorage = msg.data.compiledStorage;
+  const storage = {
+    set(data) {
+      chrome.storage.local.set({ storageData: data });
+    },
+    get(cb) {
+      chrome.storage.local.get('storageData', (storageRes) => {
+        cb(storageRes.storageData);
+      });
+    },
+  };
+
+  const events = {
+    contextBlock(data) {
+      const entries = blockHandlers[data.type](data.info);
+      globalStorage.filterData[data.type].push(...entries);
+      storage.set(globalStorage);
+    },
+    ready() {
+      utils.sendStorage();
+      ready = true;
+    },
+  };
+
+  const blockHandlers = {
+    channelId(data) {
+      return [
+        `// Blocked by context menu (${data[0].text})`,
+        data[0].navigationEndpoint.browseEndpoint.browseId,
+        '',
+      ];
+    },
+    videoId(data) {
+      return [
+        `// Blocked by context menu (${data.title.simpleText})`,
+        data.videoId,
+        '',
+      ];
+    },
+  };
+
+  const utils = {
+    sendStorage() {
+      window.postMessage({
+        from: 'BLOCKTUBE_CONTENT',
+        type: 'storageData',
+        data: compiledStorage || globalStorage,
+      }, `https://${document.domain}`);
+    },
+    inject() {
+      const s = document.createElement('script');
+      s.src = chrome.extension.getURL('src/scripts/inject.js');
+      s.onload = events.ready;
+      s.async = false;
+      (document.head || document.documentElement).appendChild(s);
+    },
+  };
+
+  // Listen for messages from background page
+  port.onMessage.addListener((msg) => {
+    switch (msg.type) {
+      case 'filtersData': {
+        if (msg.data) {
+          globalStorage = msg.data.storage;
+          compiledStorage = msg.data.compiledStorage;
+        }
+        if (ready) utils.sendStorage();
+        break;
       }
-      if (ready) utils.sendStorage();
-      break;
+      default:
+        break;
     }
-  }
-});
+  });
 
-// Listen for messages from injected page script
-window.addEventListener('message', (event) => {
-  if (event.source !== window) return;
-  if (!event.data.from || event.data.from !== 'BLOCKTUBE_PAGE') return;
+  // Listen for messages from injected page script
+  window.addEventListener('message', (event) => {
+    if (event.source !== window) return;
+    if (!event.data.from || event.data.from !== 'BLOCKTUBE_PAGE') return;
 
-  switch (event.data.type) {
-    case 'contextBlockData': {
-      events.contextBlock(event.data.data);
-      break;
+    switch (event.data.type) {
+      case 'contextBlockData': {
+        events.contextBlock(event.data.data);
+        break;
+      }
+      default:
+        break;
     }
-    case 'ready': {
-      events.ready();
-      break;
-    }
-  }
-});
+  }, true);
 
-// Inject script to page
-utils.inject();
+  // Inject script to page
+  utils.inject();
+}());
